f395a2fcc066b2f215568ee729866e3b7abb43f1
ValeriaEO <113623864+ValeriaEO@users.noreply.github.com>
2022-09-15 18:40:55 +0200

    Modificació dec2bin
    Eliminació de l'anotació inicial

--- dec2bin.py
+++ dec2bin.py
@@ -1,10 +1,4 @@
-# ------------------------------------------------------
-# Convierte un número decimal positivo a binario usando un 
-#   determinado número de bits.
-# El binario resultante es un string e.g. "101"
-# Se usa la función bin() que transforma e.g. 3 en "0b11".
-# En esta función se quita el "0b" para dejar el "11"
-# ------------------------------------------------------
+
 def dec2bin(numero_decimal, numero_bits):
     numero_binario = bin(numero_decimal)
     numero_binario = numero_binario[2:len(numero_binario)]  # quita el "0b" del principio
